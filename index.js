f844517c8971e5c3bfac245b70e76e8597ba8d1b
Jonathan Donaldson <jdonaldson278@gmail.com>
2019-05-19 15:35:34 +0100

    Added docker

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const B2 = require('backblaze-b2');
 const moment = require('moment');
@@ -5,16 +6,31 @@ const NodeCache = require('node-cache');
 const myCache = new NodeCache({ stdTTL: 6000, checkperiod: 12000 });
 
 const app = express();
-const port = 3000;
 
 var b2;
-var bucketID;
-var bucketName;
+
+const bucketID = process.env.bucketID;
+const bucketName = process.env.bucketName;
+const downloadURL = process.env.downloadURL;
+const accountId = process.env.keyID;
+const applicationKey = process.env.applicationKey;
+
+console.log(accountId);
+console.log(applicationKey);
+
+app.listen(8080, async function() {
+	b2 = new B2({
+		accountId: accountId,
+		applicationKey: applicationKey,
+	});
+	await b2.authorize();
+});
 
 app.get('*', async function(req, res) {
 	console.log(req.path);
 	try {
-		let address = await getAddress(req.path.slice(1));
+		var fileNameWithPath = req.path.slice(1);
+		let address = await getAddress(fileNameWithPath);
 		res.redirect(address);
 	} catch (err) {
 		if (err == 404 || err == 410) {
@@ -23,32 +39,35 @@ app.get('*', async function(req, res) {
 	}
 });
 
-app.listen(port, async function() {
-	await init();
-});
+function createAuthAddress(fileName, auth) {
+	return (
+		downloadURL +
+		'/' +
+		bucketName +
+		'/' +
+		fileName +
+		'?Authorization=' +
+		auth
+	);
+}
 
 function getAddress(fileName) {
 	value = myCache.get(fileName);
 
 	if (value != undefined) {
-		return value;
+		return Promise.resolve(value);
 	} else {
 		return getAuthForFileName(fileName).then(function(auth) {
-			let address =
-				'https://files.jdon.uk/file/' +
-				bucketName +
-				'/' +
-				fileName +
-				'?Authorization=' +
-				auth;
+			let address = createAuthAddress(fileName, auth);
+			console.log(address);
 			myCache.set(fileName, address);
 			return address;
 		});
 	}
 }
 
-async function getAuthForFileName(fileName) {
-	let fileInfo = await getFileInfo(fileName);
+async function getAuthForFileName(fileName, data) {
+	let fileInfo = await getFile(fileName, true);
 
 	let timeUpload = fileInfo['x-bz-upload-timestamp'];
 	let new_date = moment.unix(timeUpload).add(7, 'days');
@@ -67,12 +86,7 @@ async function init() {
 
 	bucketID = keys.bucketID;
 	bucketName = keys.bucketName;
-
-	b2 = new B2({
-		accountId: keys.keyID,
-		applicationKey: keys.applicationKey,
-	});
-	await b2.authorize();
+	downloadURL = keys.downloadURL;
 }
 
 async function getAuth(fileName, validDurationInSeconds) {
@@ -88,16 +102,21 @@ async function getAuth(fileName, validDurationInSeconds) {
 	}
 }
 
-async function getFileInfo(fileName) {
+async function getFile(fileName, infoOnly) {
+	let method = infoOnly ? 'head' : 'get';
 	try {
 		let response = await b2.downloadFileByName({
 			bucketName: bucketName,
 			fileName: fileName,
 			axiosOverride: {
-				method: 'head',
+				method: method,
 			},
 		});
-		return response.headers;
+		if (infoOnly) {
+			return response.headers;
+		} else {
+			response.data;
+		}
 	} catch (err) {
 		return Promise.reject('404');
 	}
---
+++ runDocker.bat
@@ -0,0 +1,1 @@
+docker run --name b2proxy -p 8080:8080 --env-file .env -d jdon/b2proxy

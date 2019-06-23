0abda9c763f0deb0ba33e097474c346044c86765
Jonathan Donaldson <jdonaldson278@gmail.com>
2019-06-23 20:20:11 +0100

    Added better error handling and refactored

---
+++ src/b2.js
@@ -0,0 +1,119 @@
+const B2 = require('backblaze-b2');
+const debug = require('debug')('B2PWrapper');
+const moment = require('moment');
+
+class b2Wrapper {
+	constructor(
+		bucketID,
+		bucketName,
+		downloadURL,
+		applicationKeyId,
+		applicationKey
+	) {
+		this.bucketID = bucketID;
+		this.bucketName = bucketName;
+		this.downloadURL = downloadURL;
+		this.applicationKeyId = applicationKeyId;
+		this.applicationKey = applicationKey;
+		this.isAuthorized = false;
+		this.b2 = null;
+	}
+
+	async init() {
+		debug(this.bucketID);
+		try {
+			let authedB2 = new B2({
+				applicationKeyId: this.applicationKeyId,
+				applicationKey: this.applicationKey,
+			});
+
+			await authedB2.authorize();
+			debug('Successfully authorized with B2');
+
+			this.b2 = authedB2;
+			this.isAuthorized = true;
+		} catch (err) {
+			debug(
+				'Unable to authorize with B2, please check your credentials!'
+			);
+			throw 'Unable to authorize with B2, please check your credentials!';
+		}
+	}
+
+	async getFile(fileName) {
+		try {
+			if (!this.isAuthorized) {
+				throw 'Not authorized, be sure to run init() first';
+			}
+			//b2 lib doesn't automatically auth, so lets just auth every call
+			await this.b2.authorize();
+
+			let response = await this.b2.downloadFileByName({
+				bucketName: this.bucketName,
+				fileName: fileName,
+				axiosOverride: {
+					method: 'head',
+				},
+			});
+			
+
+			let headers = response.headers;
+
+			if(!headers['x-bz-upload-timestamp']){
+				throw {
+					status: 404,
+					message: 'File not found',
+				};
+			}
+
+			// check that file was upload within the last week
+			let timeUpload = headers['x-bz-upload-timestamp'] / 1000;
+			let new_date = moment.unix(timeUpload).add(7, 'days');
+			let diffSeconds = new_date.diff(moment(), 'seconds');
+
+			if (diffSeconds < 1) {
+				throw {
+					status: 410,
+					message: 'The requested file is no longer available',
+				};
+			}
+			
+			debug(diffSeconds);
+			let authResponse = await this.b2.getDownloadAuthorization({
+				bucketId: this.bucketID,
+				fileNamePrefix: fileName,
+				validDurationInSeconds: diffSeconds,
+			});
+
+			let authData = authResponse.data;
+
+			if(!authData['authorizationToken']){
+				throw {
+					status: 500,
+					message: 'Failed to get authorization',
+				};
+			}
+
+
+			let authToken = authData.authorizationToken;
+
+			return (
+				this.downloadURL +
+				'/' +
+				this.bucketName +
+				'/' +
+				fileName +
+				'?Authorization=' +
+				authToken
+			);
+		} catch (err) {
+			debug(err);
+			throw {
+				status: err.status || 500,
+				message: err.message || err,
+			};
+		}
+	}
+}
+
+module.exports = b2Wrapper;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 const debug = require('debug')('B2Proxy');
 const express = require('express');
-const B2 = require('backblaze-b2');
-const moment = require('moment');
+
 const NodeCache = require('node-cache');
 const myCache = new NodeCache({ stdTTL: 6000, checkperiod: 12000 });
 
-const app = express();
+const b2Wrapper = require('./b2');
 
-var b2;
+const app = express();
 
 const port = process.env.port;
 const bucketID = process.env.bucketID;
@@ -17,119 +16,38 @@ const downloadURL = process.env.downloadURL;
 const applicationKeyId = process.env.keyID;
 const applicationKey = process.env.applicationKey;
 
-b2 = new B2({
-	applicationKeyId: applicationKeyId,
-	applicationKey: applicationKey,
-});
+debug(applicationKeyId);
 
-app.get('*', function(req, res) {
+let b2 = new b2Wrapper(
+	bucketID,
+	bucketName,
+	downloadURL,
+	applicationKeyId,
+	applicationKey
+);
+
+app.get('*', async function(req, res) {
 	debug(req.path);
-	let headers = req.headers;
-	if (headers['x-forwarded-for']) {
-		debug(headers['x-forwarded-for']);
-	}
-	var fileNameWithPath = req.path.slice(1);
-	let address = myCache.get(fileNameWithPath);
-
+	const fileNameWithPath = req.path.slice(1);
+	const address = myCache.get(fileNameWithPath);
 	if (address != undefined) {
 		return res.redirect(address);
-	} else {
-		return b2
-			.authorize()
-			.then(function() {
-				return getAddress(fileNameWithPath).then(function(address) {
-					return res.redirect(address);
-				});
-			})
-			.catch(function(err) {
-				res.send(err.message);
-				debug(err.message);
-			});
+	}
+	try {
+		let authURL = await b2.getFile(fileNameWithPath);
+		myCache.set(fileNameWithPath, authURL);
+		return res.redirect(authURL);
+	} catch (err) {
+		res.send(err);
 	}
 });
 
-app.listen(port, function() {
-	b2.authorize()
-		.then(function() {
-			debug('Successfully authorized with B2');
-		})
-		.catch(function(err) {
-			debug(
-				'Unable to authorize with B2, please check your credentials!'
-			);
-			process.exit(err);
-		});
+app.listen(port, async function() {
+	try {
+		await b2.init();
+	} catch (err) {
+		debug(err);
+		// eslint-disable-next-line no-process-exit
+		process.exit(err);
+	}
 });
-
-function createAuthAddress(fileName, auth) {
-	return (
-		downloadURL +
-		'/' +
-		bucketName +
-		'/' +
-		fileName +
-		'?Authorization=' +
-		auth
-	);
-}
-
-function getAddress(fileName) {
-	return getAuthForFileName(fileName).then(function(auth) {
-		let address = createAuthAddress(fileName, auth.authorizationToken);
-		myCache.set(fileName, address, auth.validDurationInSeconds);
-		return address;
-	});
-}
-
-function getAuthForFileName(fileName, data) {
-	return getFile(fileName, true).then(function(fileInfo) {
-		let timeUpload = fileInfo['x-bz-upload-timestamp'] / 1000;
-		let new_date = moment.unix(timeUpload).add(7, 'days');
-		let diffSeconds = new_date.diff(moment(), 'seconds');
-		let fileID = fileInfo['x-bz-file-id'];
-		if (diffSeconds > 1) {
-			return getAuth(fileName, diffSeconds);
-		} else {
-			return Promise.reject({
-				response: {
-					status: 410,
-				},
-				message: 'Request failed with status code 410',
-			});
-		}
-	});
-}
-
-function getAuth(fileName, validDurationInSeconds) {
-	return b2
-		.getDownloadAuthorization({
-			bucketId: bucketID,
-			fileNamePrefix: fileName,
-			validDurationInSeconds: validDurationInSeconds,
-		})
-		.then(function(response) {
-			response.data.validDurationInSeconds = validDurationInSeconds;
-			return response.data;
-		});
-}
-
-function getFile(fileName, infoOnly) {
-	let method = infoOnly ? 'head' : 'get';
-	return b2
-		.downloadFileByName({
-			bucketName: bucketName,
-			fileName: fileName,
-			axiosOverride: {
-				method: method,
-			},
-		})
-		.then(function(response) {
-			if (infoOnly) {
-				return response.headers;
-			} else {
-				return response.data;
-			}
-		});
-}
-
-function getAddr(fileNameWithPath, res) {}

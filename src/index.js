0b72d2c5fb89da530794f9eaa8a3d0e0287c5d62
Jonathan Donaldson <jdonaldson278@gmail.com>
2019-06-25 23:55:09 +0100

    Added a routes

--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
 require('dotenv').config();
-const debug = require('debug')('B2Proxy');
 const express = require('express');
-
-const NodeCache = require('node-cache');
-const myCache = new NodeCache({ stdTTL: 6000, checkperiod: 12000 });
-
-const b2Wrapper = require('./b2');
-
 const app = express();
+var router = express.Router()
+const b2Router = require('./routers/b2');
 
 const port = process.env.port;
-const bucketID = process.env.bucketID;
-const bucketName = process.env.bucketName;
-const downloadURL = process.env.downloadURL;
-const applicationKeyId = process.env.keyID;
-const applicationKey = process.env.applicationKey;
-const authSeconds = process.env.authSeconds;
 
-let b2 = new b2Wrapper(
-	bucketID,
-	bucketName,
-	downloadURL,
-	applicationKeyId,
-	applicationKey,
-	authSeconds
-);
+app.listen(port);
 
-app.get('*', async function(req, res) {
-	debug(req.path);
-	const fileNameWithPath = req.path.slice(1);
-	const address = myCache.get(fileNameWithPath);
-	if (address != undefined) {
-		return res.redirect(address);
-	}
-	try {
-		let authURL = await b2.getFile(fileNameWithPath);
-		myCache.set(fileNameWithPath, authURL);
-		return res.redirect(authURL);
-	} catch (err) {
-		res.send(err);
-	}
-});
+router.use('/', b2Router)
 
-app.listen(port, async function() {
-	try {
-		await b2.init();
-	} catch (err) {
-		debug(err);
-		// eslint-disable-next-line no-process-exit
-		process.exit(err);
-	}
-});
+app.use('/', router);
---
+++ src/routers/b2.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const b2Router = express.Router();
+const debug = require('debug')('B2Proxy-B2Router');
+const b2Wrapper = require('../b2');
+const cacheService = require('../cacheService');
+const cache = new cacheService(3600); // Create a new cache service instance
+
+const bucketID = process.env.bucketID;
+const bucketName = process.env.bucketName;
+const downloadURL = process.env.downloadURL;
+const applicationKeyId = process.env.keyID;
+const applicationKey = process.env.applicationKey;
+const authSeconds = process.env.authSeconds;
+
+let b2 = new b2Wrapper(
+	bucketID,
+	bucketName,
+	downloadURL,
+	applicationKeyId,
+	applicationKey,
+	authSeconds
+);
+
+b2Router.get('*', async (req, res) => {
+	debug(req.path);
+	const fileNameWithPath = req.path.slice(1);
+	const cachedResponse = cache.get(fileNameWithPath);
+	if(cachedResponse != null){
+		return res.redirect(cachedResponse);
+	}
+	try {
+		let authURL = await b2.getFile(fileNameWithPath);
+		cache.set(fileNameWithPath, authURL)
+		return res.redirect(authURL);
+	} catch (err) {
+		return res.send(err);
+	}
+})
+module.exports = b2Router;
